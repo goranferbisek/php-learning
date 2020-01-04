85a0a12ed33b16a4c8f75b1f545a813012f72eb4
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-01-04 11:37:43 +0100

    Refactor the router class

--- core/Router.php
+++ core/Router.php
@@ -4,6 +4,15 @@ class Router
 {
     protected $routes = [];
 
+    public static function load($file)
+    {
+        /* get instance of itself */
+        $router = new static;   // use static or self keyword
+        require $file;
+
+        return $router;
+    }
+
     public function define($routes)
     {
         $this->routes = $routes;
@@ -11,7 +20,6 @@ class Router
 
     public function direct($uri)
     {
-        //example.com/about/culture
         if (array_key_exists($uri, $this->routes)) {
             return $this->routes[$uri];
         }
--- index.php
+++ index.php
@@ -2,10 +2,8 @@
 
 $query = require 'core/bootstrap.php';
 
-$router = new Router;
-
-require 'routes.php';
-
 $uri = trim($_SERVER['REQUEST_URI'],'/');
 
+
+$router = Router::load('routes.php');
 require $router->direct($uri);
