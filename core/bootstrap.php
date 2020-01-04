74f7bb1e97710a6660c488c53a754005d165041e
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-01-04 12:23:43 +0100

    Refactor bootstrap by adding an app variable containing configuration.

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,12 +1,14 @@
 <?php
 
+$app = [];
+$app['config'] = require 'config.php';
+
 require 'core/Router.php';
 require 'core/Request.php';
-$config = require 'config.php';
 require 'core/database/Connection.php';
 require 'core/database/QueryBuilder.php';
 
 
-return new QueryBuilder(
-    Connection::make($config['database'])
+$app['database'] = new QueryBuilder(
+    Connection::make($app['config']['database'])
 );
